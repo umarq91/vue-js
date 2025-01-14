e5e7f59fa4533ded54f38ea9b45c2afdf2ec6cfe
Umar qureshi <m.umarqureshi091@gmail.com>
2025-01-14 13:02:49 +0500

    basic crud

---
+++ src/components/App2.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+const data = ref(['arham', 'arham2', 'arham3'])
+const newName = ref('')
+const addData = () => {
+  const dat = newName.value
+  data.value.push(dat)
+  newName.value = ''
+}
+
+const delteData = (index) => {
+    data.value.splice(index,1)
+}
+</script>
+
+<template>
+  <input class="px-3 py-2 border border-black m-3" v-model="newName" />
+  <button class="px-3 py-2" @click="addData">Add</button>
+
+  <div class="px-3 py-2 bg-gray-600" v-for="(name, index) in data" :key="name">
+    {{ name.toUpperCase() }}
+    <button 
+    @click="delteData(index)"
+    class="bg-red-500 px-3 py-1"> x </button>
+  </div>
+
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-};
+}
