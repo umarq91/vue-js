6386b4f4f249065011a3a2cefc7c0b521422f7b6
Umar qureshi <m.umarqureshi091@gmail.com>
2025-01-16 19:53:50 +0500

    routing added

---
+++ project1/src/components/Showname.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  heading: {
+    type: String,
+    required: true
+  },
+})
+</script>
+
+<template>
+    <h1> Hey,{{ heading }}</h1>
+  
+</template>
---
+++ quiz/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter';
+import TheWelcome from '../components/TheWelcome.vue'
+const counterStore = useCounterStore();
+</script>
+
+<template>
+  <main>
+    {{counterStore.count}}
+    <TheWelcome />
+  </main>
+</template>
